e769a06fd462cc9a6941be6e4d3ff491d1b2f1fd
juanjesuslopez <lopezsolanojuanjesus@gmail.com>
2026-03-03 12:14:17 +0100

    actualizado

--- ApiPujas/Controllers/ProductController.cs
+++ ApiPujas/Controllers/ProductController.cs
@@ -8,7 +8,7 @@ namespace ApiPujas.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase // Usamos ControllerBase para APIs
+    public class ProductController : ControllerBase
     {
         private readonly AppDbContext _context;
         private ResponseDto _response;
@@ -24,7 +24,6 @@ namespace ApiPujas.Controllers
         {
             try
             {
-                // Filtramos por fecha y mezclamos aleatoriamente
                 var products = _context.Products
                     .Where(p => p.End_date > DateTime.Now)
                     .AsEnumerable()
@@ -42,5 +41,38 @@ namespace ApiPujas.Controllers
             }
             return _response;
         }
+
+        [HttpPost]
+        public async Task<ResponseDto> Post([FromBody] Product product)
+        {
+            try
+            {
+                // Ignoramos la validación de objetos de relación
+                ModelState.Remove("User");
+                ModelState.Remove("Bids");
+
+                if (!ModelState.IsValid)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "Datos de entrada inválidos";
+                    return _response;
+                }
+
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+
+                _response.Data = product;
+                _response.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
     }
+
 }
